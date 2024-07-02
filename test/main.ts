9ae3fd38ad1295f4998c9689b92345648b0bc423
Liam Gruber <main@dnascanner.de>
2024-07-02 15:23:13 +0200

    Added display thing

--- test/main.ts
+++ test/main.ts
@@ -1,15 +1,40 @@
+const voltage = 0.5;
+
+const wattage: Record<string, number> = {
+	optimized: 0,
+	unoptimized: 0,
+};
+
+const calculateWattage = (wattage: number) => wattage * voltage;
+
 Deno.serve((req: Request) => {
 	let path = new URL(req.url).pathname;
-	path === "/" && (path = "/index");
+	path === "/" && (path = "/index.html");
 
-	switch (path) {
-		case "/on":
-                  console.log(`update: ${req.url}`);
-                  return new Response("update");
+	// Input current GET /optimized/0.00/unoptimized/0.00
 
-            default:
-                  console.log(`${path}`);
-                  return new Response("404", {status: 404});
+	const regex = /\/optimized\/(\d+\.\d+)\/unoptimized\/(\d+\.\d+)/;
+
+	if (req.method === "GET" && regex.test(path)) {
+		const [, optimized, unoptimized] = path.match(regex) as RegExpMatchArray;
+
+		// Calculate wattage
+		wattage.optimized = calculateWattage(parseFloat(optimized));
+		wattage.unoptimized = calculateWattage(parseFloat(unoptimized));
+
+		console.log(wattage);
+
+		return new Response("");
+	} else if (req.method === "GET" && path === "/data") {
+		return new Response(JSON.stringify(wattage), {status: 200, headers: {"Content-Type": "application/json"}});
+	} else if (req.method === "GET") {
+		// Try to find the file if present
+		try {
+			return new Response(Deno.readTextFileSync(`.${path}`), {status: 200, headers: {"Content-Type": "text/html"}});
+		} catch {
+			return new Response("Not found", {status: 404});
+		}
+	} else {
+		return new Response("Not found", {status: 404});
 	}
 });
-
